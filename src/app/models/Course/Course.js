73e3a7e144c4c1456bfe717a9f7be9fe4d41b2ae
hunga516 <hunga5160@gmail.com>
2024-09-20 18:15:56 +0700

    update code

--- src/app/models/Course/Course.js
+++ src/app/models/Course/Course.js
@@ -6,13 +6,14 @@ import mongooseSequence from 'mongoose-sequence';
 
 const CourseSchema = new mongoose.Schema({
     courseId: { type: Number },
-    role: { type: String },
-    name: { type: String, default: "Default name" }, //default will enable if type === undefined
+    role: { type: String, default: 'User' },
+    title: { type: String, default: "Default title" }, //default will enable if type === undefined
     description: { type: String, default: "Default description" },
     images: { type: String, default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC5V5g68dTYlES7tgY55eWdnJSChHU27m1kg&s" },
     chapters: [{ type: Schema.Types.ObjectId, ref: "Chapter" }],
     author: { type: Schema.Types.ObjectId, ref: "Users" },
     slug: { type: String, slug: 'name', unique: true },
+    registrationCount: { type: Number, default: 0 },
     deleted: { type: Boolean, default: false },
 }, {
     timestamps: true,
