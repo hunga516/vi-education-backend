c82ef2865e68b5988f49a9657c1497749644e567
hunga516 <hunga5160@gmail.com>
2024-10-17 00:14:01 +0700

    complete manage users

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -41,12 +41,15 @@ class UserController {
     }
 
     async getAllUsers(req, res, next) {
-        const { sort = "_id", order, email, page = 1 } = req.query
+        const { sort = "_id", order, email, username, page = 1 } = req.query
         const skip = (page - 1) * 10
 
         const query = { isDeleted: false }
         if (email) {
             query.email = new RegExp(email, 'i')
+        }
+        if (username) {
+            query.username = new RegExp(username, 'i')
         }
         try {
             const users = await Users.find(query).skip(skip).limit(10).sort({ [sort]: order || -1 })
@@ -89,6 +92,90 @@ class UserController {
             res.json(userEdited)
         } catch (error) {
             next(error)
+        }
+    }
+
+    async getUsersTrash(req, res, next) {
+        const { page = 1 } = req.query
+        const skip = (page - 1) * 10
+
+        try {
+            const users = await Users.find({ isDeleted: true }).skip(skip).limit(10).sort({ deletedAt: -1 })
+            const totalUsers = await Users.find({ isDeleted: true }).countDocuments()
+
+            res.json({
+                users,
+                totalUsers
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async softDeleteUser(req, res, next) {
+        try {
+            const user = await Users.findOneAndUpdate({ _id: req.params.id }, {
+                isDeleted: true,
+                deletedBy: req.body.userId,
+                deletedAt: new Date()
+            })
+
+            req.io.emit('user:soft-delete', user)
+            res.json(user)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async restoreUser(req, res, next) {
+        try {
+            const user = await Users.findOneAndUpdate({ _id: req.params.id }, {
+                isDeleted: false,
+                deletedAt: null,
+                deletedBy: null
+            })
+
+            req.io.emit('user:restore', user)
+            res.json(user)
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async handleFormAction(req, res, next) {
+        try {
+            switch (req.body.action) {
+                case 'soft-delete':
+                    await Users.updateMany({ _id: { $in: req.body.userIds } }, {
+                        isDeleted: true,
+                        deletedBy: req.body.userId,
+                        deletedAt: new Date()
+                    });
+
+                    const userDeleteds = await Users.find({ _id: { $in: req.body.userIds } })
+                    console.log(userDeleteds);
+                    req.io.emit('user:soft-delete', userDeleteds)
+                    break;
+                case 'restore':
+                    await Users.updateMany({ _id: { $in: req.body.userIds } }, {
+                        isDeleted: false,
+                        deletedAt: null,
+                        deletedBy: null
+                    });
+                    const userRestoreds = await Users.find({ _id: { $in: req.body.userIds } })
+                    req.io.emit('user:restore', userRestoreds)
+                    break;
+                case 'forceDelete':
+                    await Users.deleteMany({ _id: { $in: req.body.userIds } });
+                    res.redirect('back');
+                    break;
+                default:
+                    res.json('Invalid action!!!');
+                    break;
+            }
+        } catch (error) {
+            next(error);
         }
     }
 }
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -3,8 +3,12 @@ import userController from '../app/controllers/UserController.js';
 
 const router = express.Router();
 
+router.post('/handle-form-action', userController.handleFormAction)
 router.post('/sign-in', userController.signIn)
 router.post('/sign-up', userController.signUp)
+router.get('/trash', userController.getUsersTrash)
+router.put('/restore/:id', userController.restoreUser)
+router.delete('/:id', userController.softDeleteUser)
 router.get('/:id', userController.getUserById)
 router.put('/:id', userController.editUser)
 router.get('/email/:email', userController.getUserByEmail)
