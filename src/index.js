33872eca83597d7c852efd0e56a69277d2a11346
Ngoc Loc <hunga5160@gmail.com>
2024-11-04 21:04:00 +0700

    link to fe url

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         // origin: 'http://localhost:3000',
-        origin: 'http://localhost:3000',
+        origin: 'https://vi-education.vercel.app',
         methods: ['GET', 'POST']
     },
 })
